ad0dc2f9c986c332dd4adbd69848d201a1505384
Jaywestty <juwonfadairo10@gmail.com>
2026-07-23 12:51:38 +0100

    fix: added the missing List import

--- generation/routes.py
+++ generation/routes.py
@@ -3,6 +3,7 @@
 import time
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
 from pydantic import BaseModel
 
 from app.database import get_db
